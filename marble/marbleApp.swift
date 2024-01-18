3c034196e25a06fb5ddc067df864a01128629d0e
timoteahu <hutimk@gmail.com>
2024-01-17 22:37:38 -0500

    Initial Commit

---
+++ marble/marbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  marbleApp.swift
+//  marble
+//
+//  Created by Timothy Hu on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct marbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
